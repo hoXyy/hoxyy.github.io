79e6342c029688bcf9f6614cb43eef3244a3b0ff
hoXyy <hoxiii@pm.me>
2022-03-29 13:12:43 +0200

    Small fixes to the script

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -1,2 +1,2 @@
 @echo off
-npm run build&&cd dist&&git init&&git add -A&&git commit -m 'deploy'&&git push -f https://github.com/hoXyy/hoxyy.github.io.git main:gh-pages&&cd ..
+npm run build&&cd ..&&cd dist&&git init&&git add -A&&git commit -m 'deploy'&&git push -f https://github.com/hoXyy/hoxyy.github.io.git main:gh-pages&&cd ..
